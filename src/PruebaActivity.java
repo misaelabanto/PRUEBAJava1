375b91bfc0c98c1a286ae64d87f66d0ef6c58560
José Misael <josemisaelabanto@gmail.com>
2017-06-24 01:59:08 -0500

    Tercer commit, amigos. Ahora sí parece un proyecto de Software

--- src/PruebaActivity.java
+++ src/PruebaActivity.java
@@ -10,5 +10,7 @@ public class PruebaActivity {
     public static void main(String[] args) {
         hacerAlgo();
         System.out.println("Hola amiguitos :v");
+        System.out.println("Si yo imprimo alguna otra cosa, ¿saldrá en el mismo sitio?");
+        System.out.println("Si yo imprimo alguna otra cosa, ¿saldrá en el mismo sitio?");
     }
 }
