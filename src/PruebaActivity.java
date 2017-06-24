a9feff08149d45ce085b1850d80e4dd4467b070c
José Misael <josemisaelabanto@gmail.com>
2017-06-24 01:55:22 -0500

    Initial commit

---
+++ src/PruebaActivity.java
@@ -0,0 +1,14 @@
+/**
+ * Created by José Misael on 29/04/2017.
+ */
+public class PruebaActivity {
+
+    public static void hacerAlgo(){
+        System.out.println("4534435789");
+    }
+
+    public static void main(String[] args) {
+        hacerAlgo();
+        System.out.println("Hola amiguitos :v");
+    }
+}
