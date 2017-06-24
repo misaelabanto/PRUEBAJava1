1fb6072cdbf205341daef77eb9ab6b685875c995
José Misael <josemisaelabanto@gmail.com>
2017-06-24 02:15:28 -0500

    Último commit, amigos. Ahora sí parece un proyecto de Software

--- src/PruebaActivity.java
+++ src/PruebaActivity.java
@@ -10,6 +10,7 @@ public class PruebaActivity {
     public static void main(String[] args) {
         hacerAlgo();
         System.out.println("Hola amiguitos :v");
+        System.out.println("Este entra para el commit final. :v");
         System.out.println("Si yo imprimo alguna otra cosa, ¿saldrá en el mismo sitio?");
     }
 }
